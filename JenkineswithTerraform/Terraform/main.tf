7077c6564500c92f7052925c54848e0fce5fecf2
srilatha71 <srilatha.devi71@gmail.com>
2025-11-24 16:31:37 +0530

    Create main.tf

---
+++ JenkineswithTerraform/Terraform/main.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# ------------------------
+# VPC
+# ------------------------
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# ------------------------
+# Subnet
+# ------------------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# ------------------------
+# IGW
+# ------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+# ------------------------
+# Route Table
+# ------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# ------------------------
+# Security Group
+# ------------------------
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ------------------------
+# EC2 INSTANCE
+# ------------------------
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0e99a2cc376f2e400"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "terraform-app-server"
+  }
+}
+
+# ------------------------
+# OUTPUTS
+# ------------------------
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
