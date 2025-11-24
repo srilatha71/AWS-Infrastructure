80670e5cdff19872ffe9b6e68022a545bbeb72e5
srilatha71 <srilatha.devi71@gmail.com>
2025-11-24 17:15:10 +0530

    Delete scripts directory

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install nginx -y
-
-# Copy project to nginx html folder
-sudo cp -r /home/ubuntu/sample.app/* /var/www/html/
-
-sudo systemctl restart nginx
-echo "Deployment Completed"
